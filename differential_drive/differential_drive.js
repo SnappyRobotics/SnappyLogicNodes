bc074c8e3064144c155ac38bd1ca4868ee7c0663
belavatagi <adarsh.sb18@gmail.com>
2017-01-27 18:39:08 +0530

    Added basic differential drive node

---
+++ differential_drive/differential_drive.js
@@ -0,0 +1,33 @@
+module.exports = function(RED) {
+    "use strict";
+
+    function differential(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.turn = Number(config.turn);
+        node.motion = Number(config.motion);
+
+        this.on('input', function(msg) {
+            var newMsg = null;
+            if (msg.hasOwnProperty('turn')) {
+                node.turn = Number(msg.turn);
+            }
+            if (msg.hasOwnProperty('motion')) {
+                node.motion = Number(msg.motion);
+            } else if (msg.topic == 'turn') {
+                node.turn = Number(msg.payload);
+            } else(msg.topic == 'motion') {
+                node.motion = Number(msg.payload);
+            }
+
+            newMsg = cal();
+            node.send(newMsg);
+        });
+
+        function cal() {
+
+        }
+        RED.nodes.registerType("differential", differential);
+    }
+}
