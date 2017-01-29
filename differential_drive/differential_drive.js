cc6dbd52f927b1284fb0b25fa867cf6777cbbc9a
belavatagi <adarsh.sb18@gmail.com>
2017-01-29 12:07:50 +0530

    differential_drive done

--- differential_drive/differential_drive.js
+++ differential_drive/differential_drive.js
@@ -5,7 +5,7 @@ module.exports = function(RED) {
 
         this.on('input', function(msg) {
 
-            // msg.payload = msg.payload.toLowerCase();
+
             if (msg.topic == "turn") {
                 if (msg.payload == "left") {
                     var Lmotor = {
@@ -14,43 +14,55 @@ module.exports = function(RED) {
                     var Rmotor = {
                         payload: "anticlockwise"
                     };
-
                 } else if (msg.payload == "right") {
                     var Lmotor = {
                         payload: "clockwise"
                     };
+
                     var Rmotor = {
                         payload: "clockwise"
                     };
-
                 }
-                this.send([Lmotor, Rmotor, null]);
+                this.send([
+                    [Lmotor],
+                    [Rmotor],
+                    [null]
+                ]);
             }
-
-
             if (msg.topic == "motion") {
                 if (msg.payload == "forward") {
                     var Lmotor = {
                         payload: "anticlockwise"
                     };
+
                     var Rmotor = {
                         payload: "clockwise"
                     };
 
-                } else if (msg.payload == "revers") {
+                } else if (msg.payload == "reverse") {
                     var Lmotor = {
                         payload: "clockwise"
                     };
+
                     var Rmotor = {
                         payload: "anticlockwise"
                     };
+
                 }
-                this.send([Lmotor, Rmotor, null]);
+                this.send([
+                    [Lmotor],
+                    [Rmotor],
+                    [null]
+                ]);
             }
 
             if (msg.topic == "speed") {
 
-                this.send([null, null, msg]);
+                this.send([
+                    [null],
+                    [null],
+                    [msg]
+                ]);
             }
 
         });
