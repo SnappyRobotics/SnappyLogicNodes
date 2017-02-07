5b95abd1d52ed7d81d8cbf44095b260c7295af80
belavatagi <adarsh.sb18@gmail.com>
2017-02-07 09:57:54 +0530

    added test

---
+++ test/_spec.js
@@ -0,0 +1,73 @@
+var should = require("should");
+var helper = require('./helper.js');
+var differential_drive = require("../differential_drive/differential_drive.js");
+//var awsNode = require("../../aws/aws.js");
+//var sinon = require('sinon');
+var nock = helper.nock;
+
+describe('testing differential drive', function() {
+
+    before(function(done) {
+        helper.startServer(done);
+    });
+
+    afterEach(function() {
+        if (nock) {
+            nock.cleanAll();
+        }
+        helper.unload();
+    });
+
+    it('should be loaded', function(done) {
+        var flow = [{
+            id: "n1",
+            type: "differential drive",
+            name: "function"
+        }];
+        helper.load(differential_drive, flow, function() {
+            var n1 = helper.getNode("n1");
+            done();
+        });
+    });
+
+
+
+    /*it('checking', function(done) {
+        var flow = [{
+                id: "n1",
+                type: "differential drive",
+                wires: [
+                    ["n2"]
+                ],
+                func: "return msg;"
+            },
+            {
+                id: "n2",
+                type: "helper"
+            }
+        ];
+        helper.load(differential_drive, flow, function() {
+            var n1 = helper.getNode("n1");
+            var n2 = helper.getNode("n2");
+            var n3 = helper.getNode("n2");
+            n2.on("input", function(msg) {
+                msg.should.have.property('topic', 'bar');
+                msg.should.have.property('payload', -1);
+
+                done();
+            });
+            n3.on("input", function(msg) {
+                msg.should.have.property('topic', 'bar');
+                msg.should.have.property('payload', 1);
+
+                done();
+            });
+            n1.receive({
+                payload: "left",
+                topic: "turn"
+            });
+        });
+
+
+    });*/
+});
