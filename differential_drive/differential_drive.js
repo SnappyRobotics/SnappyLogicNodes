d73ca8b4627be87ad33463cafcbd9bb5ce73f568
belavatagi <adarsh.sb18@gmail.com>
2017-02-08 10:52:26 +0530

    added break

--- differential_drive/differential_drive.js
+++ differential_drive/differential_drive.js
@@ -4,15 +4,15 @@ module.exports = function(RED) {
     var node = this;
 
     this.on('input', function(msg) {
-      if (msg.topic == "turn") {
-        if (msg.payload == "left") {
+      if (msg.topic.toLowerCase() == "turn") {
+        if (msg.payload.toLowerCase() == "left") {
           var Lmotor = {
             payload: 1
           };
           var Rmotor = {
             payload: 1
           };
-        } else if (msg.payload == "right") {
+        } else if (msg.payload.toLowerCase() == "right") {
           var Lmotor = {
             payload: -1
           };
@@ -26,8 +26,8 @@ module.exports = function(RED) {
           [Rmotor]
         ]);
       }
-      if (msg.topic == "motion") {
-        if (msg.payload == "forward") {
+      if (msg.topic.toLowerCase() == "motion") {
+        if (msg.payload.toLowerCase() == "forward") {
           var Lmotor = {
             payload: -1
           };
@@ -36,7 +36,7 @@ module.exports = function(RED) {
             payload: 1
           };
 
-        } else if (msg.payload == "reverse") {
+        } else if (msg.payload.toLowerCase() == "reverse") {
           var Lmotor = {
             payload: 1
           };
@@ -51,6 +51,23 @@ module.exports = function(RED) {
           [Rmotor]
         ]);
       }
+      if (msg.payload.toLowerCase() == "break") {
+        var Lmotor = {
+          payload: 0
+        };
+
+        var Rmotor = {
+          payload: 0
+        };
+        this.send([
+          [Lmotor],
+          [Rmotor]
+        ]);
+
+
+      }
+
+
     });
   }
   RED.nodes.registerType("differential drive", differential_drive);
