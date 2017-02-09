735298917019cecb6853cb534e4ca943bd35fb76
belavatagi <adarsh.sb18@gmail.com>
2017-02-09 17:35:24 +0530

    working first output pin

--- DcMotor/DcMotor.js
+++ DcMotor/DcMotor.js
@@ -1,11 +1,3 @@
-var serialport = require('serialport');
-//var createNodebotNode = require('nodebotNode');
-
-var five = require('johnny-five');
-var vm = require('vm');
-var util = require('util');
-var _ = require('lodash');
-
 function connectingStatus(n) {
   n.status({
     fill: "red",
@@ -58,8 +50,6 @@ function init(RED) {
     this.state = n.state;
     this.arduino = n.arduino;
     this.nodebot = RED.nodes.getNode(n.board);
-    this.i2cAddress = parseInt(n.i2cAddress, 10);
-    this.i2cRegister = parseInt(n.i2cRegister, 10);
     if (typeof this.nodebot === "object") {
       var node = this;
       connectingStatus(node);
@@ -84,68 +74,6 @@ function init(RED) {
               if ((msg.payload == false) || (msg.payload == 0) || (msg.payload.toString().toLowerCase() === "off")) {
                 io.digitalWrite(node.pin, 0);
               }
-            } else if (state === 'PWM') {
-              try {
-                io.pinMode(node.pin, io.MODES[state]);
-              } catch (exp) {
-                console.log(exp);
-              }
-              msg.payload = msg.payload * 1;
-              if ((msg.payload >= 0) && (msg.payload <= 255)) {
-                //console.log(msg.payload, node.pin);
-                io.analogWrite(node.pin, msg.payload);
-              }
-            } else if (state === 'SERVO') {
-              try {
-                io.pinMode(node.pin, io.MODES[state]);
-              } catch (exp) {
-                console.log(exp);
-              }
-              msg.payload = msg.payload * 1;
-              if ((msg.payload >= 0) && (msg.payload <= 180)) {
-                //console.log(msg.payload, node.pin);
-                io.servoWrite(node.pin, msg.payload);
-              }
-            } else if (node.state === 'I2C_READ_REQUEST') {
-              var register = parseInt(msg.i2cRegister, 10) || parseInt(node.i2cRegister, 10);
-              var i2cAddress = parseInt(msg.i2cAddress, 10) || parseInt(node.i2cAddress, 10);
-              var numBytes = parseInt(msg.payload, 10);
-              if (io.i2cReadOnce && i2cAddress && numBytes) {
-                if (register) {
-                  io.i2cReadOnce(i2cAddress, register, numBytes, function(data) {
-                    node.send({
-                      payload: data,
-                      register: register,
-                      i2cAddress: i2cAddress,
-                      numBytes: numBytes
-                    });
-                  });
-                } else {
-                  io.i2cReadOnce(i2cAddress, numBytes, function(data) {
-                    node.send({
-                      payload: data,
-                      i2cAddress: i2cAddress,
-                      numBytes: numBytes
-                    });
-                  });
-                }
-              }
-            } else if (node.state === 'I2C_WRITE_REQUEST') {
-              var register = parseInt(msg.i2cRegister, 10) || parseInt(node.i2cRegister, 10);
-              var i2cAddress = parseInt(msg.i2cAddress, 10) || parseInt(node.i2cAddress, 10);
-              if (io.i2cWrite && i2cAddress && msg.payload) {
-                if (register) {
-                  io.i2cWrite(i2cAddress, register, msg.payload);
-                } else {
-                  io.i2cWrite(i2cAddress, msg.payload);
-                }
-              }
-            } else if (node.state === 'I2C_DELAY') {
-              if (io.i2cConfig) {
-                if (register) {
-                  io.i2cConfig(parseInt(msg.payload, 10));
-                }
-              }
             }
           } catch (inputExp) {
             node.warn(inputExp);
@@ -162,42 +90,10 @@ function init(RED) {
         ioErrorStatus(node, err);
       });
     } else {
-      this.warn("nodebot1 not configured");
+      this.warn("nodebot not configured");
     }
 
   }
-
   RED.nodes.registerType("Dc Motor", DcMotor);
-
-  function handleRoute(req, res, handler) {
-    handler(req.query)
-      .then(function(data) {
-        res.send(data);
-      }, function(err) {
-        console.log('error in gpio request', err);
-        res.send(500);
-      });
-  }
-
-  function listArduinoPorts(callback) {
-    return serialport.list(function(err, ports) {
-      if (err) {
-        return callback(err);
-      }
-      var devices = [];
-      for (var i = 0; i < ports.length; i++) {
-        if (/usb|acm|com\d+/i.test(ports[i].comName)) {
-          devices.push(ports[i].comName);
-        }
-      }
-      return callback(null, devices);
-    });
-  }
-  //routes
-  RED.httpAdmin.get("/gpioserialports", RED.auth.needsPermission("arduino.read"), function(req, res) {
-    listArduinoPorts(function(err, ports) {
-      res.json(ports);
-    });
-  });
 }
 module.exports = init;
