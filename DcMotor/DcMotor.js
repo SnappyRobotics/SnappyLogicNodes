a31471fa698165cf4efc765d89bb08fe05cfcfde
belavatagi <adarsh.sb18@gmail.com>
2017-02-10 14:16:38 +0530

    lot of cleaning

--- DcMotor/DcMotor.js
+++ DcMotor/DcMotor.js
@@ -43,14 +43,10 @@ function connectedStatus(n) {
 
 
 function init(RED) {
-  //createNodebotNode(RED);
-
   function DcMotor(n) {
     RED.nodes.createNode(this, n);
-    this.buttonState = -1;
-    this.pinA = n.pinA;
-    this.pinB = n.pinB;
-    this.state = n.state;
+    this.pinA = parseInt(n.pinA);
+    this.pinB = parseInt(n.pinB);
     this.arduino = n.arduino;
     this.nodebot = RED.nodes.getNode(n.board);
     if (typeof this.nodebot === "object") {
@@ -61,32 +57,24 @@ function init(RED) {
 
         connectedStatus(node);
 
-        debug(node.pinB);
-
         node.on('input', function(msg) {
           try {
-            var state = msg.state || node.state;
             var io = node.nodebot.io;
-            if (state === 'OUTPUT') {
-              try {
-                io.pinMode(node.pinA, io.MODES[state]);
-                io.pinMode(node.pinB, io.MODES[state]);
-              } catch (exp) {
-                console.log(exp);
-              }
-              if ((msg.payload == true) || (msg.payload == 1) || (msg.payload.toString().toLowerCase() === "on")) {
-                io.digitalWrite(node.pinA, 1);
-                io.digitalWrite(node.pinB, 1);
-              }
-              if ((msg.payload == false) || (msg.payload == 0) || (msg.payload.toString().toLowerCase() === "off")) {
-                io.digitalWrite(node.pinA, 0);
-                io.digitalWrite(node.pinB, 0);
-              }
 
+            io.pinMode(node.pinA, io.MODES["OUTPUT"]);
+            io.pinMode(node.pinB, io.MODES["OUTPUT"]);
 
+            if ((msg.payload == true) || (msg.payload == 1) || (msg.payload.toString().toLowerCase() === "on")) {
+              io.digitalWrite(node.pinA, 1);
+              io.digitalWrite(node.pinB, 1);
             }
+            if ((msg.payload == false) || (msg.payload == 0) || (msg.payload.toString().toLowerCase() === "off")) {
+              io.digitalWrite(node.pinA, 0);
+              io.digitalWrite(node.pinB, 0);
+            }
+
           } catch (inputExp) {
-            node.warn(inputExp);
+            node.error(inputExp);
           }
         });
       });
