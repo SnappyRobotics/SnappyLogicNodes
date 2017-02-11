5c95718e25175afe28adf5ca174c6a5328f9efbf
prajankya <prajankya@gmail.com>
2017-02-11 19:42:45 +0530

    bug fixes, loading container as dep

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const mocha = require('gulp-mocha');
 const gutil = require('gulp-util');
 const debug = require('debug')('SnappyLogicNodes:gulpfile');
 
-gulp.task('watch', function() {
-  gulp.watch(['differential_drive/**', 'test/**'], ['mocha-watch']);
+gulp.task('watch', function () {
+  gulp.watch(['differential_drive/**', 'test/**'], gulp.series('mocha-watch'))
 });
 
-gulp.task('mocha', function(done) {
+gulp.task('mocha', function (done) {
   debug("running mocha")
   return gulp.src(['test/**/*_spec.js'], {
       read: false
@@ -16,14 +16,14 @@ gulp.task('mocha', function(done) {
       reporter: 'spec'
     }))
     .on('error', gutil.log)
-    .on('end', function() {
+    .on('end', function () {
       debug("mocha done")
       done();
       process.exit()
     })
 });
 
-gulp.task('mocha-watch', function(done) {
+gulp.task('mocha-watch', function (done) {
   debug("running mocha")
   return gulp.src(['test/**/*_spec.js'], {
       read: false
@@ -32,7 +32,7 @@ gulp.task('mocha-watch', function(done) {
       reporter: 'spec'
     }))
     .on('error', gutil.log)
-    .on('end', function() {
+    .on('end', function () {
       debug("mocha done")
     })
 });
--- run.js
+++ run.js
@@ -1,14 +1,23 @@
 var spawn = require('child_process').spawn,
   ls = spawn('node-red');
+const debug = require('debug')('SnappyLogicNodes:run');
 
-ls.stdout.on('data', function(data) {
+const red = require('nodered_container');
+
+debug(red)
+red.getRED(__dirname, function (r) {
+  debug(r);
+})
+/*
+ls.stdout.on('data', function (data) {
   console.log('stdout: ' + data.toString());
 });
 
-ls.stderr.on('data', function(data) {
+ls.stderr.on('data', function (data) {
   console.log('stderr: ' + data.toString());
 });
 
-ls.on('exit', function(code) {
+ls.on('exit', function (code) {
   console.log('child process exited with code ' + code.toString());
 });
+*/
