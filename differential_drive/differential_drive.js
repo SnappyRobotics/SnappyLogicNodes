6f3543600dce999ad822315f9f2b92e72b16a77b
belavatagi <adarsh.sb18@gmail.com>
2017-03-02 10:13:37 +0530

    cleared bugs @differential_driveNode

--- differential_drive/differential_drive.js
+++ differential_drive/differential_drive.js
@@ -5,17 +5,17 @@ module.exports = function(RED) {
     this.motion = parseInt(config.motion);
     this.turn = parseInt(config.turn);
     this.on('input', function(msg) {
-      msg.payload = config.turn
-      msg.payload = config.motion
-      if (msg.topic.toLowerCase() == "turn" || config.turn) {
-        if (msg.payload.toLowerCase() == "left") {
+      // msg.payload = config.turn
+      // msg.payload = config.motion
+      if ((msg.topic.toLowerCase() == "turn") || (config.turn)) {
+        if ((msg.payload.toLowerCase() == "left") || (config.turn.toLowerCase() == "left")) {
           var Lmotor = {
             payload: 1
           };
           var Rmotor = {
             payload: 1
           };
-        } else if (msg.payload.toLowerCase() == "right" || config.motion) {
+        } else if ((msg.payload.toLowerCase() == "right") || (config.turn.toLowerCase() == "right")) {
           var Lmotor = {
             payload: -1
           };
@@ -29,8 +29,8 @@ module.exports = function(RED) {
           [Rmotor]
         ]);
       }
-      if (msg.topic.toLowerCase() == "motion") {
-        if (msg.payload.toLowerCase() == "forward") {
+      if ((msg.topic.toLowerCase() == "motion") || (config.motion)) {
+        if ((msg.payload.toLowerCase() == "forward") || (config.motion.toLowerCase() == "forward")) {
           var Lmotor = {
             payload: -1
           };
@@ -39,7 +39,7 @@ module.exports = function(RED) {
             payload: 1
           };
 
-        } else if (msg.payload.toLowerCase() == "reverse") {
+        } else if ((msg.payload.toLowerCase() == "reverse") || (config.motion.toLowerCase() == "reverse")) {
           var Lmotor = {
             payload: 1
           };
