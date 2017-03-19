4e544b89ca896ea72c0aff0731515121b4f1aa66
prajankya <prajankya@gmail.com>
2017-03-19 12:27:41 +0530

    solved issue with travis build,due to removed deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,10 @@
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
-const gutil = require('gulp-util');
-const path = require('path');
-const nodered_container = require('nodered_container');
+const path = require('path')
+const gulp = require('gulp')
+const mocha = require('gulp-mocha')
 
-const debug = require('debug')('SnappyLogicNodes:gulpfile');
+const debug = require('debug')('SnappyLogicNodes:gulpfile')
 
-gulp.task('installRED', function (done) {
-  var dir = __dirname;
-
-  nodered_container.check(dir, function (err) {
-    if (err) {
-      throw err
-    }
-    debug("Done")
-    done()
-  });
-})
-
-gulp.task('watch', function () {
-  gulp.watch(['differential_drive/**', 'test/**'], ['mocha-watch'])
-});
-
-gulp.task('mocha', function (done) {
+gulp.task('mocha', function(done) {
   debug("running mocha")
   return gulp.src(['test/**/*_spec.js'], {
       read: false
@@ -30,25 +12,12 @@ gulp.task('mocha', function (done) {
     .pipe(mocha({
       reporter: 'spec'
     }))
-    .on('error', gutil.log)
-    .on('end', function () {
+    .on('error', function(e) {
+      debug(e)
+    })
+    .on('end', function() {
       debug("mocha done")
       done();
       process.exit()
-      process.exit()
-    })
-});
-
-gulp.task('mocha-watch', function (done) {
-  debug("running mocha")
-  return gulp.src(['test/**/*_spec.js'], {
-      read: false
-    })
-    .pipe(mocha({
-      reporter: 'spec'
-    }))
-    .on('error', gutil.log)
-    .on('end', function () {
-      debug("mocha done")
     })
 });
