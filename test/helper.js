1cc8db84923a529de124f694712ed967c11cb060
prajankya <prajankya@gmail.com>
2017-03-19 11:46:12 +0530

    updated path for node-red

--- test/helper.js
+++ test/helper.js
@@ -17,7 +17,8 @@
 var path = require('path');
 
 process.env.NODE_RED_HOME = process.env.NODE_RED_HOME ||
-  path.join(__dirname, "..", "..", "node-red");
+  path.join(__dirname, "..", "node_modules", "node-red");
+
 var helper = require(path.join(process.env.NODE_RED_HOME,
   'test', 'nodes', 'helper.js'));
 
