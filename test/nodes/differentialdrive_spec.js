7f2e497c35b6069be8ec0e0314d9e75c3e227f12
prajankya <prajankya@gmail.com>
2017-03-19 16:20:21 +0530

    bug fix, lingering process(It was http server)

--- test/nodes/differentialdrive_spec.js
+++ test/nodes/differentialdrive_spec.js
@@ -12,6 +12,10 @@ describe('testing differential drive', function() {
     helper.unload();
   });
 
+  after(function(done) {
+    helper.stopServer(done);
+  });
+
   it('should be loaded', function(done) {
     var flow = [{
       id: "n1",
