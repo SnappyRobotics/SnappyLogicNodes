b822156d70c15f9fb082cd3717e428a25a2b0a3d
prajankya <prajankya@gmail.com>
2017-03-19 14:06:25 +0530

    added coverage, testing on travis

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 const path = require('path')
 const gulp = require('gulp')
 const mocha = require('gulp-mocha')
-const istanbul = require('gulp-istanbul');
+const istanbul = require('gulp-istanbul')
+const coveralls = require('gulp-coveralls')
 
-const debug = require('debug')('SnappyLogicNodes:gulpfile')
+const debug = require('debug')('snappy:logic:gulpfile')
 
 
 gulp.task('pre-test', function() {
-  return gulp.src(['test/**/*_spec.js'])
+  return gulp.src(['differential_drive/**/*.js'])
     // Covering files
     .pipe(istanbul())
-    // Force `require` to return covered files
-    .pipe(istanbul.hookRequire());
+    .pipe(istanbul.hookRequire())
+  // Force `require` to return covered files
 });
 
 gulp.task('mocha', ['pre-test'], function(done) {
@@ -23,13 +24,32 @@ gulp.task('mocha', ['pre-test'], function(done) {
       reporter: 'spec'
     }))
     // Creating the reports after tests ran
-    .pipe(istanbul.writeReports())
+    .pipe(istanbul.writeReports({
+      dir: './coverage',
+      reporters: ['lcov'],
+      reportOpts: {
+        dir: './coverage'
+      }
+    }))
     .on('error', function(e) {
       debug(e)
     })
     .on('end', function() {
       debug("mocha done")
-      done();
-      process.exit()
+      //  done()
     })
 });
+gulp.task('coverage', ['mocha'], function(done) {
+  debug("running coverage")
+  gulp.src('coverage/**/lcov.info')
+    .pipe(coveralls())
+    .on('end', function() {
+      debug("coverage sent to coverall")
+      done()
+      //process.exit()
+    })
+    .on('error', function(e) {
+      debug("coverage cannot be sent to coverall", e)
+      process.exit(e)
+    })
+})
