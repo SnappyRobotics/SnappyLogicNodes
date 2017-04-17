a8aaa4485794b243efec7e48ab2084600102a2f7
prajankya <prajankya@gmail.com>
2017-04-17 18:34:30 +0530

    updated payload testing

--- locomotion/differential_drive.js
+++ locomotion/differential_drive.js
@@ -1,13 +1,34 @@
 'use strict';
 
+const debug = require('debug')('snappy:logic:differential_drive');
+
 module.exports = function(RED) {
-  var LowerCaseNode = function(config) {
+  var diffNode = function(config) {
     RED.nodes.createNode(this, config);
     var node = this;
     this.on('input', function(msg) {
-      msg.payload = msg.payload.toLowerCase();
-      node.send(msg);
+      if (msg.payload &&
+        msg.payload.linear &&
+        msg.payload.linear.x != null &&
+        msg.payload.angular &&
+        msg.payload.angular.z != null) {
+
+        node.send(msg)
+
+      } else {
+        node.status({
+          fill: "red",
+          shape: "dot",
+          text: "wrong input"
+        })
+        setTimeout(function() {
+          node.status({})
+        }, 3000);
+      }
+
+      debug(msg)
+
     });
   }
-  RED.nodes.registerType("differential-drive", LowerCaseNode);
+  RED.nodes.registerType("differential-drive", diffNode);
 }
