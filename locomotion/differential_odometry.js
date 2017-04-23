9b7bdaedb949b528cc07492ebd3cc0bdae5ceeb9
prajankya <prajankya@gmail.com>
2017-04-23 15:22:37 +0530

    added differential-odometry

--- locomotion/differential_odometry.js
+++ locomotion/differential_odometry.js
@@ -6,117 +6,133 @@ module.exports = function(RED) {
   var diffOdomNode = function(config) {
     RED.nodes.createNode(this, config);
     var node = this;
-    var outputs = []
 
-    // -255 to 255 divided by smoothness
-    var step = (255 * 2 / config.smoothness || 10);
-
-    var cur_right = 0;
-    var cur_left = 0;
-
-    outputs.push({ //RIGHT
-      payload: {
-        speed: 0,
-        brake: true
-      }
-    })
-
-    outputs.push({ //LEFT
-      payload: {
-        speed: 0,
-        brake: true
-      }
-    })
-
-    var constrain = function(num) {
-      if (num < -255) {
-        return -255;
-      } else if (num > 255) {
-        return 255;
-      } else {
-        return num;
+    var output = {
+      "pose": {
+        "position": {
+          "x": 0,
+          "y": 0,
+          "z": 0
+        },
+        "orientation": {
+          "x": 0,
+          "y": 0,
+          "z": 0
+        }
+      },
+      "twist": {
+        "linear": {
+          "x": 0,
+          "y": 0,
+          "z": 0
+        },
+        "angular": {
+          "x": 0,
+          "y": 0,
+          "z": 0
+        }
       }
     }
 
-    var right_speed_out = 0
-    var left_speed_out = 0
-    var runningUpdate = false
+    var o = {
+      oldL: 0,
+      dl: 0,
+      l: 0,
 
-    var updateSpeed = function(u) {
-      if (u && runningUpdate) {
-        return
-      }
-      runningUpdate = true
-      // debug(right_speed_out, cur_right)
-      if (right_speed_out != cur_right || left_speed_out != cur_left) {
-        if (Math.abs(right_speed_out - cur_right) > step) {
-          if (right_speed_out > cur_right) {
-            cur_right += step
-          } else if (right_speed_out < cur_right) {
-            cur_right -= step
-          }
-        } else {
-          cur_right = right_speed_out
-        }
+      oldR: 0,
+      dr: 0,
+      r: 0,
 
-        if (Math.abs(left_speed_out - cur_left) > step) {
-          if (left_speed_out > cur_left) {
-            cur_left += step
-          } else if (left_speed_out < cur_left) {
-            cur_left -= step
-          }
+      oldT: Date.now(),
+      newT: Date.now(),
+      dt: 0,
 
-        } else {
-          cur_left = left_speed_out
-        }
+      x: 0,
+      dx: 0,
+      vx: 0,
 
-        cur_right = Math.round(cur_right)
-        cur_left = Math.round(cur_left)
+      y: 0,
+      dy: 0,
+      vy: 0,
 
-        outputs[0].payload.speed = Math.round(cur_right)
-        outputs[1].payload.speed = Math.round(cur_left)
-
-        // debug('speed out:', cur_right, cur_left)
-
-        node.send(outputs)
-        setTimeout(function() {
-          updateSpeed()
-        }, 10);
-      } else {
-        runningUpdate = false
-      }
+      dth: 0,
+      th: 0,
+      vth: 0
     }
 
+    var checkNum = function(x) {
+      if (isNaN(x) || !isFinite(x)) {
+        return 0
+      } else {
+        return x
+      }
+    }
 
     var processInput = function(msg) {
-      var x = msg.payload.linear.x * config.gain
-      var z = msg.payload.angular.z * config.gain
-
-      // debug('x, z :', x, z)
-      right_speed_out = Math.round(constrain(x + (z * config.wheel_distance / 2)))
-      left_speed_out = Math.round(constrain(x - (z * config.wheel_distance / 2)))
-
-
-      if (x === 0 && z === 0) {
-        right_speed_out = 0
-        left_speed_out = 0
-        outputs[0].payload.brake = true
-        outputs[1].payload.brake = true
-      } else {
-        outputs[0].payload.brake = false
-        outputs[1].payload.brake = false
+      if (msg.topic == config.left_topic) {
+        o.l = msg.payload.travel
       }
-      if (right_speed_out != cur_right || left_speed_out != cur_left) {
-        updateSpeed(1)
+
+      if (msg.topic == config.right_topic) {
+        o.r = msg.payload.travel
+      }
+
+      if (o.oldL != o.l || o.oldR != o.r) {
+        o.newT = Date.now()
+
+        o.dl = o.l - o.oldL
+        o.dr = o.r - o.oldR
+        o.dt = (o.newT - o.oldT) / 1000 //In seconds
+
+        //http://rossum.sourceforge.net/papers/DiffSteer/
+        var s_ = (o.dl + o.dr) / 2
+
+        o.dth = (o.dl - o.dr) / (config.wheel_distance)
+
+        o.dx = s_ * Math.cos(o.th + o.dth) //th+dth because th+=dth is to be done after this function is executed
+        o.dy = s_ * Math.sin(o.th + o.dth)
+
+        o.vx = o.dx / o.dt
+        o.vy = o.dy / o.dt
+        o.vth = o.dth / o.dt
+
+        o.x += o.dx
+        o.y += o.dy
+        o.th += o.dth
+
+        if (o.th > Math.PI) {
+          o.th = o.th - (2 * Math.PI)
+        } else if (o.th < -Math.PI) {
+          o.th = o.th + (2 * Math.PI)
+        }
+
+        output.pose.position.x = checkNum(o.x)
+        output.pose.position.y = checkNum(o.y)
+
+        const quaternion = require('quaternion');
+        var q = new quaternion;
+        q.setFromAxisAngle([0, 0, 1], checkNum(o.th) * Math.PI / 180)
+
+        output.pose.orientation.x = checkNum(q.x)
+        output.pose.orientation.y = checkNum(q.y)
+        output.pose.orientation.z = checkNum(q.z)
+        output.pose.orientation.w = checkNum(q.w)
+
+        output.twist.linear.x = checkNum(o.vx)
+        output.twist.linear.y = checkNum(o.vy)
+        output.twist.angular.z = checkNum(o.vth)
+
+        node.send([{
+          payload: output
+        }])
+        o.oldT = o.newT
+      } else {
+        return
       }
     }
 
     this.on('input', function(msg) {
-      if (msg.payload &&
-        msg.payload.linear &&
-        msg.payload.linear.x !== undefined &&
-        msg.payload.angular &&
-        msg.payload.angular.z !== undefined) {
+      if (msg.payload && msg.payload.travel !== undefined) {
 
         processInput(msg)
       } else {
